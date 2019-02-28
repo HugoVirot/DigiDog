b7b4fc4f01c9b4bbedf3590743c8107a8f01ab4c
Hugo Virot <hugo.virot@gmail.com>
2019-02-28 16:38:04 +0100

    dernière version avec quelques ajouts

--- DigiDog_/resources/views/pages/welcome.blade.php
+++ DigiDog_/resources/views/pages/welcome.blade.php
@@ -8,4 +8,9 @@
 
 @section('content')
     <p>Bienvenue chez DigiDog !</p>
+
+    <h2>VOTRE CHIEN AUSSI AIME LA TECHNOLOGIE !</h2>
+    <p>DigiDog : un large choix d'objets connectés pour chien.
+        Facilitez-vous la vie, et celle de votre compagnon à quatre pattes !</p>
+
 @endsection
