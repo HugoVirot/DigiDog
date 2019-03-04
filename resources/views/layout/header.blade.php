61a4ab122870dfc2be8c62345dc3726d2ad8784a
antoineGranjon <antoine.granjon@gmail.com>
2019-03-04 13:47:58 +0100

    change directory of project

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,42 @@
+<header>
+
+    <div class="container-fluid row align-items-end ">
+
+        <div class="col-6">
+            <img src="images/logo-horizontal.png" class="logo-header img-fluid">
+        </div>
+
+        <div class="col-6">
+            <nav class="navbar navbar-light bg-primary navbar-expand-lg rounded-pill justify-content-center mb-4 menu">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01"
+                        aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+
+
+
+
+                    <a class="nav-link" href="#">Accueil</a>
+                    <a class="nav-link" href="#">Catalogue</a>
+                    <a class="nav-link" href="#">Panier</a>
+                    <a class="nav-link" href="#">Connexion</a>
+                    <a class="nav-link" href="#">A propos</a>
+                </div>
+            </nav>
+        </div>
+    </div>
+
+</header>
