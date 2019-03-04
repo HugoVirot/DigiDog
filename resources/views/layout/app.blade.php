9145116f87dbb5dc708fa2a60832a15e2a8366ca
antoineGranjon <antoine.granjon@gmail.com>
2019-03-04 15:50:11 +0100

    ajout des favicon

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <link rel="icon" href="images/logo_sans_texte.png">
 </head>
 <body class="d-flex flex-column h-100">
     @include('layout.header')
