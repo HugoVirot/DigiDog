2978be3b68c2a8f2363fc8cd0a9528b706329224
Benjamin Dechene <benjidechene@hotmail.com>
2019-03-04 15:14:26 +0100

    modifsfooter

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="footer mt-auto py -3">
+<footer class="footer mt-auto py-3">
 
 
     <div class="container-fluid">
@@ -36,7 +36,7 @@
                 </ul>
             </div>
             <div class="col-md-3 text-center align-self-center">
-                <p>DIGIDOG <br>
+                <p><img src="images/logo_sans_texte.png" class="logo-footer"/>DIGIDOG <br>
                     21 rue du caniche<br>
                     37000 TOURS<br>
                     téléphone 0623072030<br>
@@ -44,7 +44,9 @@
             </div>
 
             <div class="col-md-3 text-center align-self-center">
-
+                <a target="_blank" href="_http://pinterest.com">
+                    <img src="images/pinterest.png" class="pinterest">
+                </a>
 
                 <a target="_blank" href="_http://instagram.com">
                     <img src="images/instagram.png" class="instagram">
