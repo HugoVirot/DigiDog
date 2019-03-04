764d972896acedc58b83ddfa00570891414a444d
antoineGranjon <antoine.granjon@gmail.com>
2019-03-04 15:22:39 +0100

    modif installer

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,4 +1,4 @@
-<header>
+<header class="mb-5 bg-success">
 
     <div class="container-fluid row align-items-end ">
 
@@ -12,22 +12,7 @@
                         aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-
-                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                        </li>
-                    </ul>
-
-
-
+                <div class="collapse navbar-collapse justify-content-center" id="navbarTogglerDemo01">
 
                     <a class="nav-link" href="#">Accueil</a>
                     <a class="nav-link" href="#">Catalogue</a>
