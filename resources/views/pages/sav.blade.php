3e948d93e7830c7825048b2bc1412e88de323db9
Hugo Virot <hugo.virot@gmail.com>
2019-03-06 11:29:44 +0100

    page sav terminée

--- resources/views/pages/sav.blade.php
+++ resources/views/pages/sav.blade.php
@@ -5,21 +5,22 @@
 @endsection
 
 @section('content')
-    <article id="connexion" class="container-fluid mb-3">
+    <article id="sav" class="container-fluid mb-3">
         <div class="row justify-content-center">
-            <img id="chiots" src="{{ asset("images/chiots.jpg") }}">
+            <img id="chiencle" src="{{ asset("images/chiencle.jpg") }}">
             <h1>Service après-vente</h1>
         </div>
     </article>
     <article id="formulaire" class="container justify-content-center text-center mb-3">
         <h2>Un problème avec votre produit ? Pas de souci !</h2>
-        <p>DigiDog prend en charge les retours pendant 2 ans après l'achat.</p>
-        <p>Contactez-nous au numéro suivant : </p>
-        <p>04 04 04 04 04</p>
+        <p>DigiDog prend en charge les retours <strong>pendant 2 ans après l'achat.</strong></p>
+        <p>Contactez-nous au numéro suivant<br>
+            (du lundi au samedi, de 9h00 à 20h00) :</p>
+        <p id="numero">04 04 04 04 04</p>
         <p>Si des réparations sont nécessaires, envoyez vos produits à :</p>
-        <p>Digidog - Service après-vente <br>
-            3, rue du Lévrier
-            99999 Dogville
+        <p id="numero">Digidog - Service après-vente <br>
+            3, rue du Lévrier <br>
+            99999 DOGVILLE
         </p>
     </article>
 @endsection
