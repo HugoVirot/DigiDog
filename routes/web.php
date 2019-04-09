1ff2a2db66b7505e16ae9e909e427681707800fe
Julien FAURE <julien.faure26@gmail.com>
2019-04-09 09:22:24 +0200

    authentification de la page connection

--- routes/web.php
+++ routes/web.php
@@ -37,8 +37,8 @@ Route::get('/catalogue/{product}', "ProductController@show")->name('voir_Produit
 Route::post('/login', "LoginController@index");
 
 Route::get('/mon_compte', "LoginController@show");
-Route::get('/mes_commandes', "LoginController@commandes");
-Route::get('/mes_adresses', "LoginController@adresses");
+Route::get('/mes_commandes', "LoginController@commandes")->middleware('auth');
+Route::get('/mes_adresses', "LoginController@adresses")->middleware('auth');
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/contact', "PageController@contact");
 Route::get('/recrutement', "PageController@recrutement");
