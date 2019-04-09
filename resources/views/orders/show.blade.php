78b5ebdfba052bab1232559cc99782e691884b18
marlenegomes <marlenegomes1@gmail.com>
2019-04-09 11:46:20 +0200

    modification index et show .blade >> affichage commandes

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -4,23 +4,29 @@
 
     <div class="container">
 
-        <div class="row pb-5">
-            <h1></h1>
+        <div class="row text-secondary border-bottom mb-4">
+            <h1 class="h3">Détails de votre commande</h1>
         </div>
         <div class="row">
-            <div class="col-md-4">
 
-            </div>
-            <div class="col-md-8">
+            <div class="col-md-8 pb-3">
                 <p>Numéro de commande: {{ $order->order_num }}</p>
                 <p>Date: {{ $order->date }}</p>
             </div>
             <table class="table">
+                <thead>
+                <tr>
+                    <th>Article</th>
+                    <th>Prix unitaire</th>
+                    <th>Quantité</th>
+                    <th>Prix total</th>
+                </tr>
+                </thead>
                 <tbody>
                 @foreach($order->products as $product)
                 <tr>
                     <td>{{ $product->name }}</td>
-                    <td>{{ $product->pivot->price }}</td>
+                    <td>{{ $product->pivot->price }} €</td>
                     <td>{{ $product->pivot->quantity }}</td>
                     <td>{{ ($product->pivot->quantity * $product->pivot->price) }} €</td>
                 </tr>
